f05c6d739caf91222d92e429727fea65131ded1b
scififyi <cwm@posteo.net>
2017-11-17 15:33:01 +0000

    ported scifi themes over to glitchy

---
+++ app/javascript/themes/default/default.js
@@ -0,0 +1,14 @@
+import loadPolyfills from '../../mastodon/load_polyfills';
+
+// import default stylesheet with variables
+require('font-awesome/css/font-awesome.css');
+
+import '../../styles/scifi.scss';
+
+require.context('../../images/', true);
+
+loadPolyfills().then(() => {
+  require('../../mastodon/main').default();
+}).catch(e => {
+  console.error(e);
+});
